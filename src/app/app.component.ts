5e39a393c22f48ddee783c971c273ae2f56f9e31
Symon Silva Lima <symonlima07@gmail.com>
2025-03-05 15:10:25 -0300

    Service Angular, usando api do Pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {CardComponent} from './components/card/card.component'
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from './services/pokemon.service';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CardComponent, HttpClientModule, FormsModule],
+  providers: [PokemonService],
+  template: '<h1>Aula de Services</h1> <app-card></app-card> <router-outlet></router-outlet>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,54 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonData } from '../../models/pokemonData';
+
+
+@Component({
+  selector: 'app-card',
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+
+  pokemon: PokemonData
+
+  constructor(
+    private service:PokemonService
+  ){
+    this.pokemon = {
+      id:0,
+      name:'',
+      sprites:{
+        front_default:'',
+      },
+      types:[]
+    }
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+        },
+        error: (err) => console.log('not found')
+
+      }
+    )
+  }
+
+}
